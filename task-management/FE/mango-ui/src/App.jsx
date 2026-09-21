ac65953fe83163175dede017f9badb3a9242c53d
Xoai1405 <hagiabao0506@gmail.com>
2026-09-21 13:46:57 +0700

    init: FE

---
+++ task-management/FE/mango-ui/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-slate-900 text-white">
+      <h1 className="text-3xl font-bold underline text-emerald-400">
+        Mango UI - Tailwind CSS v4 đã hoạt động!
+      </h1>
+    </div>
+  )
+}
